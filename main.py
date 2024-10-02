c828fb34dcda9fb3f5870bb1d34a019456eeb718
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2024-10-02 13:32:40 +0300

    Co-authored-by: Alisher Kabardiyadi <justcgh9@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ vid = cv2.VideoCapture(0)
 while True:
     ret, frame = vid.read()
     
-    cv2.imshow('WebCam agent', frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    frame = cv2.flip(frame, 1)
+    
+    frame = cv2.resize(frame, (600, 600), interpolation=cv2.INTER_LINEAR)
+
+    frame = cv2.blur(frame, (5, 5))
+    
+    cv2.imshow('WebCam', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
